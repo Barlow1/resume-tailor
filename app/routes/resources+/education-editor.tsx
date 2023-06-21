ab9947885ff34f0adcd7c3cfbfaa8dc179f66670
Christian Barlow <c.barlow108@gmail.com>
2023-06-21 02:37:13 -0500

    fix type/lint errors

--- app/routes/resources+/education-editor.tsx
+++ app/routes/resources+/education-editor.tsx
@@ -38,7 +38,7 @@ export async function action({ request }: DataFunctionArgs) {
 			{ status: 400 },
 		)
 	}
-	let education: { id: string; resume: { title: string; owner: User } }
+	let education: { id: string; resume: { title: string | null; owner: User } }
 
 	const {
 		school,
