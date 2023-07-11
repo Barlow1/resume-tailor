1e9ff23902b7d6b99747017196c46af03aa7fca0
Christian Barlow <c.barlow108@gmail.com>
2023-07-11 02:22:05 -0500

    fix name tests

--- tests/e2e/onboarding.test.ts
+++ tests/e2e/onboarding.test.ts
@@ -75,7 +75,7 @@ test('onboarding', async ({ page }) => {
 
 	await page.getByRole('button', { name: /Create an account/i }).click()
 
-	await expect(page).toHaveURL(`/users/${onboardingData.name}/jobs/new`)
+	await expect(page).toHaveURL(`/users/${onboardingData.username.toLowerCase()}/jobs/new`)
 
 	await page.getByRole('link', { name: onboardingData.name }).click()
 	await page.getByRole('menuitem', { name: /profile/i }).click()
@@ -100,7 +100,7 @@ test('login as existing user', async ({ page }) => {
 	await page.getByRole('textbox', { name: /username/i }).fill(user.username)
 	await page.getByLabel(/^password$/i).fill(password)
 	await page.getByRole('button', { name: /log in/i }).click()
-	await expect(page).toHaveURL(`/users/${user.name}/jobs/new`)
+	await expect(page).toHaveURL(`/users/${user.username.toLowerCase()}/jobs/new`)
 
 	await expect(page.getByRole('link', { name: user.name })).toBeVisible()
 })
@@ -153,7 +153,7 @@ test('reset password', async ({ page }) => {
 	await page.getByLabel(/^password$/i).fill(newPassword)
 	await page.getByRole('button', { name: /log in/i }).click()
 
-	await expect(page).toHaveURL(`/users/${user.username}/jobs/new`)
+	await expect(page).toHaveURL(`/users/${user.username.toLowerCase()}/jobs/new`)
 
 	await expect(page.getByRole('link', { name: user.name })).toBeVisible()
 })
