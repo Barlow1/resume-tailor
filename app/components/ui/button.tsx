875cf3e8daadefe3caae6019afec12f7a9bf985b
Christian Barlow <c.barlow108@gmail.com>
2023-08-18 01:15:52 -0500

    add generate functionality

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -22,6 +22,7 @@ const buttonVariants = cva(
 			size: {
 				default: 'h-10 px-4 py-2',
 				wide: 'px-24 py-5',
+				tall: 'py-24 px-5',
 				sm: 'h-9 rounded-md px-3',
 				lg: 'h-11 rounded-md px-8',
 				pill: 'px-12 py-3 leading-3',
--- app/routes/resources+/onboarding-stepper.tsx
+++ app/routes/resources+/onboarding-stepper.tsx
@@ -150,6 +150,29 @@ export default function OnboardingStepper({
 										onClick={dismissModal}
 										className="dark:border-night-400 relative ml-5 border-l border-gray-500 dark:border-gray-200 text-gray-500 dark:text-gray-400"
 									>
+										<Link to={`/users/${user?.username}/resume/upload`}>
+											<li className="mb-10 ml-6">
+												{gettingStartedProgress?.hasSavedResume ? (
+													<GreenCheckCircle />
+												) : (
+													<span className="dark:bg-night-400 absolute -left-4 flex h-8 w-8 items-center justify-center rounded-full bg-accent ring-4 ring-white dark:ring-gray-900">
+														<svg
+															className="h-3.5 w-3.5 text-gray-500 dark:text-gray-400"
+															aria-hidden="true"
+															xmlns="http://www.w3.org/2000/svg"
+															fill="currentColor"
+															viewBox="0 0 20 16"
+														>
+															<path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
+														</svg>
+													</span>
+												)}
+												<h3 className="font-medium leading-tight">Resume</h3>
+												<p className="text-sm">
+													Upload or enter your resume information
+												</p>
+											</li>
+										</Link>
 										<Link to={`/users/${user?.username}/jobs/new`}>
 											<li className="mb-10 ml-6">
 												{gettingStartedProgress?.hasSavedJob ? (
@@ -174,34 +197,11 @@ export default function OnboardingStepper({
 												</p>
 											</li>
 										</Link>
-										<Link to={`/users/${user?.username}/resume/upload`}>
-											<li className="mb-10 ml-6">
-												{gettingStartedProgress?.hasSavedResume ? (
-													<GreenCheckCircle />
-												) : (
-													<span className="dark:bg-night-400 absolute -left-4 flex h-8 w-8 items-center justify-center rounded-full bg-accent ring-4 ring-white dark:ring-gray-900">
-														<svg
-															className="h-3.5 w-3.5 text-gray-500 dark:text-gray-400"
-															aria-hidden="true"
-															xmlns="http://www.w3.org/2000/svg"
-															fill="currentColor"
-															viewBox="0 0 20 16"
-														>
-															<path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
-														</svg>
-													</span>
-												)}
-												<h3 className="font-medium leading-tight">Resume</h3>
-												<p className="text-sm">
-													Upload or enter your resume information
-												</p>
-											</li>
-										</Link>
 										<Link
 											to={
 												firstJob?.id
-													? `/users/${user?.username}/jobs/${firstJob?.id}/tailor`
-													: ''
+													? `/users/${user?.username}/jobs/${firstJob?.id}`
+													: `/users/${user?.username}/jobs/new`
 											}
 										>
 											<li className="mb-10 ml-6">
@@ -221,7 +221,7 @@ export default function OnboardingStepper({
 													</span>
 												)}
 												<h3 className="font-medium leading-tight">Tailor</h3>
-												<p className="text-sm">Tailor your resume</p>
+												<p className="text-sm">Tailor or generate your experience</p>
 											</li>
 										</Link>
 									</ol>
