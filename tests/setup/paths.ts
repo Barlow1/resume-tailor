5f41f55e37efc5ea7c2517a9fde973b7ac61ee73
Christian Barlow <c.barlow108@gmail.com>
2025-11-05 23:47:29 -0600

    update cache paths

--- tests/setup/paths.ts
+++ tests/setup/paths.ts
@@ -7,3 +7,6 @@ export const DATABASE_URL = `file:${DATABASE_PATH}?connection_limit=1`
 const baseDatabaseFile = `./tests/prisma/base.db`
 export const BASE_DATABASE_PATH = path.join(process.cwd(), baseDatabaseFile)
 export const BASE_DATABASE_URL = `file:${BASE_DATABASE_PATH}?connection_limit=1`
+
+const cacheFile = `./tests/prisma/cache.${process.env.VITEST_POOL_ID || 0}.db`
+export const CACHE_DATABASE_PATH = path.join(process.cwd(), cacheFile)
