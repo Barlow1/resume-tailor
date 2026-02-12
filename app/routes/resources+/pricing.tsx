f462bd59dd709a97ede3bdb70abf5e887c5fdfec
Christian Barlow <c.barlow108@gmail.com>
2026-02-12 00:38:09 -0600

    update paywall

--- app/routes/resources+/pricing.tsx
+++ app/routes/resources+/pricing.tsx
@@ -28,7 +28,8 @@ const tiers = [
 		description:
 			"Build your first resume and tailor it to any job you're applying to and export it to an ATS optimized PDF",
 		features: [
-			'18 tailored/generated experiences',
+			'6 tailored experiences',
+			'6 generated experiences',
 			'48-hour support response time',
 			'Export to an ATS optimized PDF',
 		],
