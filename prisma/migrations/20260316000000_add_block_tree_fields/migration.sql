8a55df12b5992720b262b2c5e0e01936ba5522c3
Christian Barlow <c.barlow108@gmail.com>
2026-04-01 22:45:25 -0500

    fix migrations

--- prisma/migrations/20260316000000_add_block_tree_fields/migration.sql
+++ prisma/migrations/20260316000000_add_block_tree_fields/migration.sql
@@ -3,5 +3,3 @@ ALTER TABLE "BuilderResume" ADD COLUMN "blockTree" TEXT;
 ALTER TABLE "BuilderResume" ADD COLUMN "sourceFormat" TEXT;
 ALTER TABLE "BuilderResume" ADD COLUMN "sourceFileId" TEXT;
 ALTER TABLE "BuilderResume" ADD COLUMN "templateHtml" TEXT;
-ALTER TABLE "BuilderResume" ADD COLUMN "tailorSnapshot" TEXT;
-ALTER TABLE "BuilderResume" ADD COLUMN "tailorSnapshotDate" DATETIME;
